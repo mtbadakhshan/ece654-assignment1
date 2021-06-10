5da20f41f7825bb0d12517a610ec472b95063c3a
Mohammadtaghi <mtbadakhshan@gmail.com>
2021-06-09 23:43:54 -0400

    Adds comments

--- analysis/ast/ast_identifiers.py
+++ analysis/ast/ast_identifiers.py
@@ -8,22 +8,18 @@ def get_identifiers(pyfile):
 	# https://docs.python.org/3/library/ast.html
 	for node in ast.walk(tree):
 		if hasattr(node, 'name'):
-			# print('name: ',node.name)
 			identifiers.append(node.name)
+			
 		elif hasattr(node, 'id'):
-			# print('id: ',node.id)
 			identifiers.append(node.id)
 
 		elif hasattr(node, 'attr'):
-			# print('attr: ',node.attr)
 			identifiers.append(node.attr)
 
 		elif hasattr(node, 'arg'):
-			# print('arg: ',node.arg)
 			identifiers.append(node.arg)
 
 		elif hasattr(node, 'asname'):
-			# print('asname: ',node.asname)
 			identifiers.append(node.asname)
 		
 	return set(identifiers)
--- src/authenticate.py
+++ src/authenticate.py
@@ -2,8 +2,6 @@ from hashlib import sha256
 import csv
 
 
-
-
 def gen_hashed_credentials():
 	with open ('hashed-credentials.csv', 'w') as hcred_csv:
 		h_credentials = csv.writer(hcred_csv)
@@ -13,7 +11,13 @@ def gen_hashed_credentials():
 				row[1] = sha256(row[1].encode()).hexdigest()[0:4]
 				h_credentials.writerow(row)
 
+
 def authenticate(username, password):
+# The main function: gets username and password, check if the inputed 
+# username is existed in the database(hashed-credentials.csv), then 
+# checks if the hash value of inputed password is equal to the hash 
+# value that is associated to the username stored in the database and 
+# is stored in the dataset.
 	with open('./src/hashed-credentials.csv', 'r') as hcred_csv:
 		h_credentials = csv.reader(hcred_csv)
 		for row in h_credentials:
